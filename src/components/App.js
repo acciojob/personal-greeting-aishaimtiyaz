c883eaf29a6aeee0bbebafc6d639178fecd99682
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-19 20:45:15 +0530

    commit chnges

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,30 @@ import './../styles/App.css';
 
 const App = () => {
 
-  const [name, setName] = useState("");
+ const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleBlur = () => {
+    setGreeting(`Hello, ${name}!`);
+  };
   return (
-    <div>
-        <p>Enter your name:</p>
-        <input onChange={(e) => setName(e.target.value)}></input>
-        <p>{name}</p>
+<div>
+      <label>
+        Enter your name:
+        <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          onBlur={handleBlur}
+        />
+      </label>
+      <div>{greeting}</div>
     </div>
-  )
-}
+  );
+};
 
 export default App
